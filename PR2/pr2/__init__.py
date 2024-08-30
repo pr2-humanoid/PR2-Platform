6f743700da80cd98bc4e29a2572a617cb11fc74d
Qi Xie <echoqishu@gmail.com>
2024-08-30 11:33:51 +0800

    add installation config

---
+++ PR2/pr2/__init__.py
@@ -0,0 +1,42 @@
+__version__ = "0.0.1"
+import sys
+from pathlib import Path
+
+from omni.isaac.kit import SimulationApp
+
+app = SimulationApp(
+    {"headless": False, "renderer": "RayTracedLighting", "multi_gpu": False}
+)
+
+ROOT_PATH = Path(__file__).parent
+
+
+def start():
+    # pylint: disable=import-outside-toplevel
+    from omni.isaac.core.simulation_context import SimulationContext
+    from omni.isaac.core.utils.extensions import enable_extension
+
+    enable_extension("omni.isaac.version")
+
+    simulator = SimulationContext(backend="torch", set_defaults=True)
+    # defaults settings:
+    #    [physics_dt = 1.0/ 60.0,
+    #    stage units in meters = 0.01 (i.e in cms),
+    #    rendering_dt = 1.0 / 60.0,
+    #    gravity = -9.81 m / s
+    #    ccd_enabled,
+    #    stabilization_enabled,
+    #    gpu dynamics turned off,
+    #    broadcast type is MBP,
+    #    solver type is TGS].
+    return simulator
+
+
+def shutdown():
+    # shutdown app
+    app.close()
+    # clean exit
+    sys.exit(0)
+
+
+sim = start()
